dfa7f736728cc096a80b8c7c804c8ac3839c13be
anlivl <anlivl179@gmail.com>
2021-06-14 00:59:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ class S(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print 'Starting httpd on port {}...'.format(port)
+    print("Starting httpd on port {}...".format(port))
     httpd.serve_forever()
 
 if __name__ == "__main__":
