e2b603b3d8a5f250462e2cfae120892772df56ad
anlivl <anlivl179@gmail.com>
2021-07-23 00:55:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def hello():
     return "Hello from Python! New"+ random.choice(["1","2","3"])
 
+@app.route("/read_file")
+def read_file():
+    f - open("/data/testfile.txt")
+    contents = f.read()
+    return "Text=" + contents
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
